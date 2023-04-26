5b975190ed2975fc23267085f22a5fbb22f23521
podzemgaz <podzemgaz@gmail.com>
2023-04-27 00:32:49 +0300

    first

---
+++ apptemp.js
@@ -0,0 +1,13 @@
+// import "./index.html";
+// import { ReactComponent as Icon } from "./img/contacts-button.svg";
+
+// export default function App() {
+//     return (
+//         <div className="App">
+//             <Icon className="icon" width="18" height="20" />
+//             <Icon fill="#01AE7E" width="18" height="20" />
+//             <Icon className="we-are-social"/>
+//             <Icon fill="#bf2742"/>
+//         </div>
+//     );
+// }
